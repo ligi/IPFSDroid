8c2ca8c429c186c506f97a1fdfb02ee0e3d128f2
ligi <ligi@ligi.de>
2016-05-02 17:22:36 +0200

    Add LoadToast when loading

--- app/src/main/java/org/ligi/ipfsdroid/IPFSBrowseActivity.java
+++ app/src/main/java/org/ligi/ipfsdroid/IPFSBrowseActivity.java
@@ -3,8 +3,10 @@ package org.ligi.ipfsdroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import net.steamcrafted.loadtoast.LoadToast;
 
 public class IPFSBrowseActivity extends AppCompatActivity {
 
@@ -23,10 +25,19 @@ public class IPFSBrowseActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        final LoadToast loadToast = new LoadToast(this).show();
         final IPXSResource ipxsResource = new IPXSResource(getIntent().getData());
         webView.loadUrl(ipxsResource.getIpfsioAddress());
         webView.getSettings().setJavaScriptEnabled(false);
 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(final WebView view, final String url) {
+                super.onPageFinished(view, url);
+
+                loadToast.success();
+            }
+        });
         if (getSupportActionBar() != null) {
             getSupportActionBar().setSubtitle(ipxsResource.toString());
         }
