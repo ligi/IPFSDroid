423638944028ed9049cb7b89fe5c1f5adf0e8032
ligi <ligi@ligi.de>
2016-05-09 19:36:10 +0200

    Initial with ipfs binary

---
+++ app/src/main/java/org/ligi/ipfsdroid/IPFSBinaryController.kt
@@ -0,0 +1,42 @@
+package org.ligi.ipfsdroid
+
+import android.app.AlertDialog
+import android.content.Context
+import android.os.Build
+import java.io.File
+import java.io.FileOutputStream
+
+class IPFSBinaryController(val context: Context) {
+
+    fun copy() {
+
+        if (Build.SUPPORTED_ABIS.contains("x86")) {
+            doCopyForArch("ipfs-android-16-386")
+        } else if (Build.SUPPORTED_ABIS.contains("armeabi")) {
+            doCopyForArch("ipfs-android-16-arm")
+        } else {
+            AlertDialog.Builder(context).setMessage("no supported ABIs").show()
+        }
+
+    }
+
+    private fun doCopyForArch(s: String) {
+        val out = FileOutputStream(getFile())
+        context.assets.open(s).copyTo(out)
+        out.close()
+        getFile().setExecutable(true)
+    }
+
+    fun run(cmd: String): String {
+        val process = Runtime.getRuntime().exec(getFile().absolutePath + " " + cmd)
+
+        val err = process.errorStream.reader().readText()
+
+        if (!err.isEmpty()) {
+            return err
+        }
+        return  process.inputStream.reader().readText()
+    }
+
+    fun getFile() = File(context.filesDir, "ipfs_bin")
+}
--- app/src/main/java/org/ligi/ipfsdroid/MainActivity.java
+++ app/src/main/java/org/ligi/ipfsdroid/MainActivity.java
@@ -2,15 +2,37 @@ package org.ligi.ipfsdroid;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.widget.TextView;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IPFSBinaryController ipfsBinaryInstaller;
+
+    @BindView(R.id.installButton)
+    Button installButton;
+
+    @BindViews(R.id.version)
+    List<Button> actionButtons;
+
+    @OnClick(R.id.installButton)
+    void onInstallClick() {
+        ipfsBinaryInstaller.copy();
+        refresh();
+    }
+
+    @OnClick(R.id.version)
+    void onVersionClick() {
+        new AlertDialog.Builder(this).setMessage(ipfsBinaryInstaller.run("version")).show();
+    }
 
     @OnClick(R.id.exampleButton)
     void onButtonClick() {
@@ -22,9 +44,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ipfsBinaryInstaller = new IPFSBinaryController(this);
+
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    private void refresh() {
+        installButton.setVisibility(ipfsBinaryInstaller.getFile().exists() ? View.GONE : View.VISIBLE);
+
+        final int actionButtonsVisibility = ipfsBinaryInstaller.getFile().exists() ? View.VISIBLE : View.GONE;
+
+        for (final Button actionButton : actionButtons) {
+            actionButton.setVisibility(actionButtonsVisibility);
+        }
     }
 
 }
