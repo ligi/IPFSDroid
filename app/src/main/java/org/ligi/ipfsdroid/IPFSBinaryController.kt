a2398d0374222fb9f50c17fd0658df22ac749dcd
ligi <ligi@ligi.de>
2016-05-26 00:45:20 +0200

    Improve copying the right binary for ABI

--- app/src/main/java/org/ligi/ipfsdroid/IPFSBinaryController.kt
+++ app/src/main/java/org/ligi/ipfsdroid/IPFSBinaryController.kt
@@ -1,5 +1,6 @@
 package org.ligi.ipfsdroid
 
+import android.annotation.TargetApi
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Build
@@ -9,14 +10,33 @@ import java.io.FileOutputStream
 class IPFSBinaryController(val context: Context) {
 
     fun copy() {
-
-        if (Build.CPU_ABI.equals("x86")) {
-            doCopyForArch("ipfs-android-16-386")
-        } else if (Build.CPU_ABI.equals("armeabi")) {
-            doCopyForArch("ipfs-android-16-arm")
+        val calculateFilename = calculateFilename()
+        if (calculateFilename != null) {
+            doCopyForArch(calculateFilename)
         } else {
             AlertDialog.Builder(context).setMessage("no supported ABIs").show()
         }
+    }
+
+    private fun calculateFilename(): String? {
+        if (Build.VERSION.SDK_INT >= 21) {
+            return calculateFilename21()
+        } else if (Build.CPU_ABI.equals("x86")) {
+            return "ipfs-android-16-386"
+        } else if (Build.CPU_ABI.startsWith("arm")) {
+            return "ipfs-android-16-arm"
+        }
+        return null
+    }
+
+    @TargetApi(21)
+    private fun calculateFilename21(): String? {
+        if (Build.CPU_ABI.equals("x86")) {
+            return "ipfs-android-16-386"
+        } else if (Build.CPU_ABI.startsWith("arm")) {
+            return "ipfs-android-16-arm"
+        }
+        return null
     }
 
     private fun doCopyForArch(s: String) {
@@ -27,15 +47,15 @@ class IPFSBinaryController(val context: Context) {
     }
 
     fun run(cmd: String): String {
-        val env = arrayOf("IPFS_PATH="+File(context.filesDir,".ipfs").absoluteFile)
-        val process = Runtime.getRuntime().exec(getFile().absolutePath + " " + cmd,env)
+        val env = arrayOf("IPFS_PATH=" + File(context.filesDir, ".ipfs").absoluteFile)
+        val process = Runtime.getRuntime().exec(getFile().absolutePath + " " + cmd, env)
 
         val err = process.errorStream.reader().readText()
 
         if (!err.isEmpty()) {
             return err
         }
-        return  process.inputStream.reader().readText()
+        return process.inputStream.reader().readText()
     }
 
     fun getFile() = File(context.filesDir, "ipfs_bin")
