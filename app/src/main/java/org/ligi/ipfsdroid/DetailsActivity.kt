f5c4f8ccdc7293b487f20f85113701ec8083aad3
ligi <ligi@ligi.de>
2017-10-01 09:30:43 +0200

    Use kotlin 1.1.51

--- app/src/main/java/org/ligi/ipfsdroid/DetailsActivity.kt
+++ app/src/main/java/org/ligi/ipfsdroid/DetailsActivity.kt
@@ -38,7 +38,7 @@ class DetailsActivity : AppCompatActivity() {
             addFile.visibility = View.GONE
         }
 
-        findViewById(R.id.addTextCommand)!!.setOnClickListener {
+        addTextCommand.setOnClickListener {
             val intent = Intent(this, AddIPFSContent::class.java)
             intent.action = Intent.ACTION_SEND
             intent.type = "text/plain"
@@ -46,7 +46,7 @@ class DetailsActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        findViewById(R.id.gcButton)!!.setOnClickListener {
+        gcButton.setOnClickListener {
             Thread(Runnable {
                 val gc = ipfs.repo.gc()
 
