d90e9c8abce6ee0624b3582e9ca79a12b77030b0
daniel-cook <daniel.cook@willowtreeapps.com>
2018-08-29 14:34:02 -0400

    Add functionality to download metadata and feed content files from IPFS

--- app/src/main/java/org/ligi/ipfsdroid/activities/DetailsActivity.kt
+++ app/src/main/java/org/ligi/ipfsdroid/activities/DetailsActivity.kt
@@ -1,22 +1,27 @@
 package org.ligi.ipfsdroid.activities
 
-import android.annotation.TargetApi
 import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.format.Formatter
-import android.view.View
+import android.util.Log
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.ipfs.kotlin.IPFS
 import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.coroutines.experimental.async
 import org.ligi.ipfsdroid.App
 import org.ligi.ipfsdroid.R
+import org.ligi.ipfsdroid.model.BroadCastersList
+import org.ligi.ipfsdroid.model.Feed
+import org.ligi.ipfsdroid.model.FeedsList
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
 import java.net.ConnectException
+import java.nio.charset.StandardCharsets
 import javax.inject.Inject
 
 class DetailsActivity : AppCompatActivity() {
@@ -27,6 +32,11 @@ class DetailsActivity : AppCompatActivity() {
     var running = true
     val OPEN_FILE_READ_REQUEST_CODE = 1
 
+    val ipfsHashForFeed = "QmX3AdrLhfbzgpY8CCibBpz8YnGQjzHVbQMQDCvmV5Xoey"
+    val ipnsNamespaceForFeed ="QmPEVsRj29xgpG44ZrCL1rRyCSVbVxAhU7z11UWsQNXY7o"
+
+    val TAG = DetailsActivity::class.qualifiedName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,32 +44,63 @@ class DetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_details)
         title = "IPFSDroid Info"
 
-        if (Build.VERSION.SDK_INT >= 19) {
-            wireAddFileFor19Plus()
-        } else {
-            addFile.visibility = View.GONE
-        }
+        // let's assume that we're getting a list of broadcasters from the web someplace and for now just load it as an asset
+        val feeds = loadJSONFromAsset()
+        Log.d(TAG, "Got text from Feed Asset: $feeds")
 
-        addTextCommand.setOnClickListener {
-            val intent = Intent(this, AddIPFSContentActivity::class.java)
-            intent.action = Intent.ACTION_SEND
-            intent.type = "text/plain"
-            intent.putExtra(android.content.Intent.EXTRA_TEXT, textEdit.text.toString())
-            startActivity(intent)
-        }
+        val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
 
-        gcButton.setOnClickListener {
-            Thread(Runnable {
-                val gc = ipfs.repo.gc()
 
+        val jsonAdapter = moshi.adapter(BroadCastersList::class.java)
+        val broadcaster = jsonAdapter.fromJson(feeds)
+
+        val broadCaster0 = broadcaster?.broadcasters?.get(0)
+        Log.d(TAG, "Retrieved broadcaster: " + broadCaster0.toString())
+
+        async {
+
+
+            // You'll also need to have a daemon running
+            Log.d(TAG, "Getting file from IPFS")
+
+            if(broadCaster0 != null) {
+                val jsonString = ipfs.get.cat(broadCaster0.feedHash)
+                Log.d(TAG, jsonString)
                 runOnUiThread {
-                    AlertDialog.Builder(textEdit.context)
-                            .setMessage("Collected " + gc.size + " objects")
-                            .show()
+                    broadcastersTextView.text = jsonString
                 }
-            }).start()
+
+                val feedsAdapter = moshi.adapter(FeedsList::class.java)
+                val feedsList = feedsAdapter.fromJson(jsonString)
+
+                // TODO this could result in null pointer exception
+                ipfs.pins.add(feedsList!!.content[0].file) // TODO does this actually do what I think it does?
+                val downloadFile = getDownloadFile(feedsList!!.content[0])
+                ipfs.get.catStream(feedsList!!.content[0].file) {
+                    Log.d(TAG, "Getting input streem from feedhash")
+                    downloadFile.copyInputStreamToFile(it)
+                    Log.d(TAG, "File download complete")
+
+                }
+
+            }
+
         }
 
+    }
+
+    private fun getDownloadFile(feed: Feed): File {
+        return File(this.filesDir, feed.description)
+    }
+
+    fun File.copyInputStreamToFile(inputStream: InputStream) {
+        inputStream.use { input ->
+            this.outputStream().use { fileOut ->
+                input.copyTo(fileOut)
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
@@ -72,20 +113,6 @@ class DetailsActivity : AppCompatActivity() {
                 startActivity(targetIntent)
             }
         }
-    }
-
-    @TargetApi(19)
-    private fun wireAddFileFor19Plus() {
-        addFile.setOnClickListener({
-            try {
-                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-                intent.addCategory(Intent.CATEGORY_OPENABLE)
-                intent.type = "*/*" // tried with octet stream - no use
-                startActivityForResult(intent, OPEN_FILE_READ_REQUEST_CODE)
-            } catch (e: ActivityNotFoundException) {
-                Snackbar.make(addFile, "Unavailable", Snackbar.LENGTH_LONG).show()
-            }
-        })
     }
 
     override fun onResume() {
@@ -131,4 +158,20 @@ class DetailsActivity : AppCompatActivity() {
         }).start()
     }
 
+    fun loadJSONFromAsset(): String {
+        var json: String? = null
+        try {
+            val stream = getAssets().open("feeds.json")
+            val size = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            json = String(buffer, StandardCharsets.UTF_8)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return ""
+        }
+
+        return json
+    }
 }
---
+++ app/src/main/java/org/ligi/ipfsdroid/model/Feed.kt
@@ -0,0 +1,6 @@
+package org.ligi.ipfsdroid.model
+
+/**
+ * Created by WillowTree on 8/29/18.
+ */
+data class Feed(val name: String, val description: String, val file: String)
